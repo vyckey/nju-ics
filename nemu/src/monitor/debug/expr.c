68a80d00be66bed85819701338175f021d208580
tracer-ics2017 <tracer@njuics.org>
2017-09-04 03:08:39 +0000

    >  compile U201414670 vyckey Linux 295913e18f0d 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 GNU/Linux  03:08:39 up  3:07,  4 users,  load average: 0.03, 0.07, 0.07 8a54e27f357a81738f6bbb112a7564e5f1527d3

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -266,9 +266,8 @@ uint32_t expr_cal(bool *suc, int begin, int end) {
 		if (type == TK_LP) ++par;
 		else if (type == TK_RP) --par;
 		else if (par == 0 && prior(type) < 7) {
-			printf("--%d\n",op);
 			if (op == end) op = i;
-			else if (prior(type) < prior(op)) op = i;
+			else if (prior(type) < prior(tokens[op].type)) op = i;
 		}
 	}
 	if (par == 0 && op != end) {
