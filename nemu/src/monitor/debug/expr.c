c104821c09837ad293f13fe0663a5b16e5582bde
tracer-ics2017 <tracer@njuics.org>
2017-09-05 00:41:09 +0000

    >  compile U201414670 vyckey Linux 295913e18f0d 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 GNU/Linux  00:41:09 up 28 min,  4 users,  load average: 0.22, 0.17, 0.18 a17a70447a92cbf9c81b6563b792a358eaf7c779

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -189,7 +189,7 @@ static uint32_t cal(int type, uint32_t a, uint32_t b) {
 		case TK_GE: result = a >= b; break;
 		case TK_GT: result = a > b; break;
 		case TK_REG: result = (a <= R_EDI ? reg_l(a) : cpu.eip);
-		case TK_REF: break; //result = *(int*)a;
+		case TK_REF: result = vaddr_read(a, 4); break;
 		default: ;
 	}
 	return result;
