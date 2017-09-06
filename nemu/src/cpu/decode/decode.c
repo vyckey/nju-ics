82c03929422e313da467cd2a1f8d9303ea8f0efb
tracer-ics2017 <tracer@njuics.org>
2017-09-06 11:40:56 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  11:40:56 up  3:41,  1 user,  load average: 0.65, 0.37, 0.33 6b633a8731048baa54fd313f2d652cba54dae1e3

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -71,7 +71,7 @@ static inline make_DopHelper(r) {
   op->type = OP_TYPE_REG;
   op->reg = decoding.opcode & 0x7;
   if (load_val) {
-    rtl_lr(&op->val, op->reg, op->width);printf("%d\n", op->val);
+    rtl_lr(&op->val, op->reg, op->width);
   }
 
 #ifdef DEBUG
