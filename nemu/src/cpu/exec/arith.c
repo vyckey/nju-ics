0247b64f2d4bd533e69d028fa7304da73004c755
tracer-ics2017 <tracer@njuics.org>
2017-09-06 17:21:31 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  17:21:31 up  9:21,  1 user,  load average: 1.13, 1.21, 1.33 1f377fdf471171b7e7f3ad063be2a6341c6e43c3

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -7,8 +7,13 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
+  rtlreg_t d = id_dest->val;
   rtl_sub(&id_dest->val, &id_dest->val, &id_src->val);
-
+  rtl_update_ZFSF(&id_dest->val, id_dest->width);
+  if ((d ^ id_src->val) & (~(id_src->val ^ id_dest->val)) & 0x80000000) {
+    d = 0x1;
+    rtl_set_OF(&d);
+  }
   print_asm_template2(sub);
 }
 
