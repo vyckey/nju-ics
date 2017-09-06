01eedf0f66275375a27398e3978990e67abdef83
tracer-ics2017 <tracer@njuics.org>
2017-09-06 23:32:58 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  23:32:58 up 15:33,  1 user,  load average: 1.51, 1.43, 1.32 8c304cfce1738ec3ef894618b1927dab302d28b8

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -126,17 +126,18 @@ make_rtl_setget_eflags(SF)
 
 static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
   // dest <- src1
-  TODO();
+  *dest = *src1;
 }
 
 static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
-  TODO();
+  *dest = ~ *dest;
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  width <<= 3;
+  if ((0x1 << (width - 1)) & *src1) *dest = ((-1 >> width) << width) & *src1;
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
