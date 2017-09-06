472e449b1bbfb70f0ab994ca28e9131581f85940
tracer-ics2017 <tracer@njuics.org>
2017-09-06 11:11:06 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  11:11:06 up  3:11,  1 user,  load average: 0.24, 0.38, 0.40 8fe667de1a1e8fda7ea7505eab007f526e99d6f2

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,6 +25,7 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
+  printf("%d\n", id_dest->simm);
   decoding.jmp_eip = id_dest->simm + *eip;
   decoding.is_jmp = 1;
 
