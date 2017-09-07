3cadfbfaa341a8abd03a9131549f26b0f817e176
tracer-ics2017 <tracer@njuics.org>
2017-09-07 15:01:58 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  15:01:58 up  6:46,  1 user,  load average: 0.38, 0.28, 0.15 e7be6bbd1fd5137942150f4690403936ce69c5c

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -137,8 +137,8 @@ static inline void rtl_not(rtlreg_t* dest) {
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   assert(0 < width && width <= 4);
-  width = 32 - (width << 3);printf("w%d\n", width);
-  *dest = (*src1 << width) >> width;
+  width = 32 - (width << 3);
+  *dest = (*src1 << width) >> width;printf("0x%x\n", (*src1 << width));
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
