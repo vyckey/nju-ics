e5cefeb1ffc4cb480c53ee6cb1b8814fa08708c5
tracer-ics2017 <tracer@njuics.org>
2017-09-10 10:11:47 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  10:11:47 up  1:02,  1 user,  load average: 0.56, 0.45, 0.40 ea88d9b0577676f3bc0e713d3022f8bba468fad2

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -20,8 +20,8 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_E: rtl_get_ZF(dest); break;
     case CC_BE: rtl_get_CF(dest);rtl_get_ZF(&t); *dest = *dest || t; break;
     case CC_S: rtl_get_SF(dest); break;
-    case CC_L: case CC_LE:
-      rtl_get_SF(dest); rtl_get_OF(&t); *dest = (*dest != t);
+    case CC_L: rtl_get_SF(dest); rtl_get_OF(&t); *dest = (*dest != t); break;
+    case CC_LE: rtl_get_SF(dest); rtl_get_OF(&t); *dest = (*dest != t);
       rtl_get_ZF(&t); *dest = (*dest || t); break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
