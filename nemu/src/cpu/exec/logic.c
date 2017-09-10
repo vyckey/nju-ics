5e4c3da944fdc48684fe62e36b552f40525b1421
tracer-ics2017 <tracer@njuics.org>
2017-09-10 09:44:56 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  09:44:56 up 35 min,  1 user,  load average: 0.50, 0.40, 0.32 4e7612d2a8606722149f8c2c951af06f9a6a603e

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -1,10 +1,16 @@
 #include "cpu/exec.h"
 
 static inline void logic_set_eflags(const rtlreg_t* result, int width) {
+  rtlreg_t t;
   rtl_update_ZFSF(result, width);
-  rtl_li(&t0, 0);
-  rtl_set_CF(&t0);
-  rtl_set_OF(&t0);
+  rtl_li(&t, 0);
+  rtl_set_CF(&t);
+  rtl_set_OF(&t);
+}
+
+static inline void shift_set_cf(const rtlreg_t* result, int n) {
+  rtlreg_t t = (*result >> n) & 0x1;
+  rtl_set_CF(&t);
 }
 make_EHelper(test) {
   rtl_and(&t0, &id_dest->val, &id_src->val);
@@ -38,14 +44,17 @@ make_EHelper(or) {
 }
 
 make_EHelper(sar) {
+  if (id_src->val != 0) shift_set_cf(&id_dest->val, (id_src->val & 0x1f) - 1);
   rtl_sar(&t0, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t0);
   // unnecessary to update CF and OF in NEMU
   rtl_update_ZFSF(&t0, id_dest->width);
+
   print_asm_template2(sar);
 }
 
 make_EHelper(shl) {
+  if (id_src->val != 0) shift_set_cf(&id_dest->val, 32 - (id_src->val & 0x1f));
   rtl_shl(&t0, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t0);
   // unnecessary to update CF and OF in NEMU
@@ -54,6 +63,7 @@ make_EHelper(shl) {
 }
 
 make_EHelper(shr) {
+  if (id_src->val != 0) shift_set_cf(&id_dest->val, (id_src->val & 0x1f) - 1);
   rtl_shr(&t0, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t0);
   // unnecessary to update CF and OF in NEMU
