31e68c1a1e750d6c0f4b315c32600767aacddc7f
tracer-ics2017 <tracer@njuics.org>
2017-09-10 17:41:16 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  17:41:16 up  8:31,  1 user,  load average: 0.59, 0.40, 0.31 25dfb13a1451ed39f79da1e22d156f3d2802161

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,4 +1,5 @@
 #include "nemu.h"
+#include "device/mmio.h"
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
@@ -12,11 +13,17 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+	int device_no = is_mmio(addr);
+ 	return ((device_no == -1)
+ 		? pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3))
+ 		: mmio_read(addr, len, device_no));
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+	int device_no = is_mmio(addr);
+	if (device_no == -1)
+ 		memcpy(guest_to_host(addr), &data, len);
+ 	else mmio_write(addr, len, data, device_no);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
