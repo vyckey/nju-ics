f595c75e3d62e61540156c946f61773a84780217
tracer-ics2017 <tracer@njuics.org>
2017-09-14 19:28:43 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:28:43 up 56 min,  1 user,  load average: 1.41, 1.43, 1.39 c25aea7efa96fe2034f7745693c4c8b95ccecc9d

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -164,7 +164,7 @@ void difftest_step(uint32_t eip) {
   if ((r.eflags & 0xac3) != cpu.eflags_val) {
     Log("\nQEMU --> (eip=0x%x)error in eflags!\nqemu: 0x%x, nemu: 0x%x",
         nemu_r.eip, r.eflags & 0xac3, cpu.eflags_val);
-    //diff = true;
+    diff = true;
   }
 
   if (diff) {
