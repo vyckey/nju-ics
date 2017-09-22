c28070fca42da7d04bc1a6906ca054ed24a0c877
tracer-ics2017 <tracer@njuics.org>
2017-09-22 10:25:43 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  10:25:43 up  2:27,  1 user,  load average: 0.61, 0.28, 0.28 18738f82ee02f490b857a3ad82b9b19ac38fb390

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -12,7 +12,7 @@ make_EHelper(push) {
 
 make_EHelper(pop) {
   rtl_pop(&id_dest->val);
-  operand_write(id_dest, &id_dest->val);
+  operand_write(id_dest, &id_dest->val);printf("dfsjfidaf\n");
 
   print_asm_template1(pop);
 }
