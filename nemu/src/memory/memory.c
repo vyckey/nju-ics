d0fed83d5c90b0952b40a2437239b2d10146fbee
tracer-ics2017 <tracer@njuics.org>
2017-09-22 10:15:20 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  10:15:20 up  2:17,  1 user,  load average: 0.50, 0.46, 0.36 193f3b001ed0f7e225e35867b99dff9b3a230283

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -39,7 +39,7 @@ static paddr_t page_translate(vaddr_t addr) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-	printf("%x\n", addr);
+	printf("read %#x\n", addr);
 	if (cpu.cr0 & 0x80000000) {
 		if ((addr & PAGE_MASK) + len > PAGE_SIZE) {
 			int len1, len2;
@@ -62,6 +62,7 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
+	printf("write %#x\n", addr);
 	if (cpu.cr0 & 0x80000000) {
 		if ((addr & PAGE_MASK) + len > PAGE_SIZE) {
 			int len1, len2;
