4166f772f38bff262eb6aaac11ec01b2c68f08c7
tracer-ics2017 <tracer@njuics.org>
2017-09-22 13:43:30 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  13:43:30 up  5:45,  1 user,  load average: 0.56, 0.36, 0.34 47a191e83abeb04cf46bb8501633d8b1bba05172

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -16,7 +16,7 @@ make_EHelper(lidt) {
 
 make_EHelper(mov_r2cr) {
   switch (id_dest->reg) {
-    case 0: cpu.cr0 = id_src->val;printf("--->%p\n", &cpu.cr0); break;
+    case 0: cpu.cr0 = id_src->val; break;
     case 3: cpu.cr3 = id_src->val; break;
     default: panic("Invalid CR exec");
   }t0 = 0x7ba0;
