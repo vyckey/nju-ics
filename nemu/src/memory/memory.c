9bdd39174898467a0433c3c8f6f07452cec81e4e
tracer-ics2017 <tracer@njuics.org>
2017-09-24 20:08:56 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  20:08:56 up  1:57,  1 user,  load average: 0.20, 0.29, 0.27 96511e2943d74ed91e010d47ae33ec205543e268

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -32,10 +32,10 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 static paddr_t page_translate(vaddr_t addr) {
 	PDE pde;
 	PTE pte;
-if (addr==0x8048000) printf("cr3 %x\n", cpu.cr3);
+
 	pde.val = paddr_read(PAGE_FRAME(cpu.cr3) + sizeof(PDE)*PDE_IDX(addr), sizeof(PDE));
 	if (! pde.present) panic("Invalid page directory entry at address %#x\n", addr);
-
+if (addr==0x8048000) printf("cr3 %x pte %x\n", cpu.cr3, pte.val);
 	pte.val = paddr_read(PAGE_FRAME(pde.val) + sizeof(PTE)*PTE_IDX(addr), sizeof(PTE));
 	if (! pte.present) panic("Invalid page table entry at address %#x\n", addr);
 
