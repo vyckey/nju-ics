66f6cdcae517f9fd4b020c7e22a7a36cbc3e2a3e
tracer-ics2017 <tracer@njuics.org>
2017-09-27 16:36:22 +0800

    >  compile U201414670 sunxin Linux Dell-Latitude-E7450 4.10.0-35-generic #39~16.04.1-Ubuntu SMP Wed Sep 13 09:02:42 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  16:36:22 up  1:37,  1 user,  load average: 0.37, 0.32, 0.34 a284d745318636dea66f529b6d0b79d9fe525407

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -52,7 +52,7 @@ make_EHelper(int) {
 make_EHelper(iret) {
   // no implement all, read i386
   rtl_pop(&t0);
-  decoding.jmp_eip = t0;printf("-%x\n", cpu.esp);
+  decoding.jmp_eip = t0;
   decoding.is_jmp = 1;
   rtl_pop(&t0);
   cpu.cs = t0;
